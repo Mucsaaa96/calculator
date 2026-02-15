a3f7414d064116cd11475a526df9c8448d5996b5
Mucsaaa96 <mucsaaa96@gmail.com>
2026-02-15 10:02:44 +0100

    Add querySelector to display

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 const buttons = document.querySelectorAll("button");
+const display = document.querySelector("#display");
 
 buttons.forEach(button => {
     button.addEventListener("click", (event) => {
