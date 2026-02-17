1b2993e7bb6d3ce78984749c54911adf5ff008c2
Mucsaaa96 <mucsaaa96@gmail.com>
2026-02-17 11:07:05 +0100

    Create function calculate

--- javascript.js
+++ javascript.js
@@ -25,6 +25,7 @@ function handleInput(value) {
             }
         
             const lastInArray = symbols[symbols.length - 1];
+
             if (operators.includes(lastInArray)) {
                 symbols[symbols.length - 1] = value;
             } else {
@@ -39,3 +40,23 @@ calculatorBtns.forEach(calculatorBtn => {
     calculatorBtn.addEventListener("click", (event) => handleInput(event.target.textContent))
 })
 
+function calculate(array) {
+    for(let i = 0; i < array.length; i++) {
+    if( array[i] === "x" || array[i] === "÷"){
+        const a = array[i - 1];
+        const b = array[i + 1];
+        const res = array[i] === "x" ? a * b : a / b;
+        array.splice(i - 1, 3, String(res))
+        i--;
+    }
+    } 
+    let result = Number(array[0])
+
+    for(let i = 1; i < array.length; i += 2) {
+        const op = array[i]
+        const num = Number(array[i + 1]);
+        if(op === "+") result += num;
+        else result -= num;
+    }
+    return result;
+}
